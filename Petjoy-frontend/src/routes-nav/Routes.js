91f4661903d3263a25bf90e541f0b7ec5bfabfd5
Halim Tannous <48407395+halimt92@users.noreply.github.com>
2023-04-10 22:17:21 +0200

    post session 2

--- Petjoy-frontend/src/routes-nav/Routes.js
+++ Petjoy-frontend/src/routes-nav/Routes.js
@@ -6,56 +6,55 @@ import RegisterForm from "../login/RegisterForm";
 import ProductDetail from "../product/ProductDetail";
 import Product from "../product/Product";
 import PrivateRoute from "./PrivateRoute";
+import AdminPrivateRoute from "./AdminPrivateRoute";
 import ProductCart from "../product/ProductCart";
 import ProductAdmin from "../product/ProductAdmin";
 import SavedShow from "../product/SavedShow";
 import ProfileForm from "../users/ProfileForm";
 
 function Routes({ login, signup }) {
-
   return (
-      <div>
-        <Switch>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
 
-          <Route exact path="/">
-            <Homepage />
-          </Route>
+        <Route exact path="/login">
+          <LoginForm login={login} />
+        </Route>
 
-          <Route exact path="/login">
-            <LoginForm login={login} />
-          </Route>
+        <Route exact path="/signup">
+          <RegisterForm signup={signup} />
+        </Route>
 
-          <Route exact path="/signup">
-            <RegisterForm signup={signup} />
-          </Route>
+        <Route exact path="/products/:username/saved">
+          <SavedShow />
+        </Route>
 
-          <Route exact path="/products/:username/saved">
-          <SavedShow/>
-          </Route>
+        <Route exact path="/products">
+          <Product />
+        </Route>
 
-          <Route exact path="/products">
-            <Product/>
-          </Route>
+        <PrivateRoute exact path="/products/cart">
+          <ProductCart />
+        </PrivateRoute>
 
-          <PrivateRoute exact path="/products/cart">
-            <ProductCart/>
-          </PrivateRoute>
+        <AdminPrivateRoute exact path="/products/admin">
+          <ProductAdmin />
+        </AdminPrivateRoute>
 
-          <PrivateRoute exact path="/products/admin">
-            <ProductAdmin/>
-          </PrivateRoute>
+        <PrivateRoute exact path="/products/:id">
+          <ProductDetail />
+        </PrivateRoute>
 
-          <PrivateRoute exact path="/products/:id">
-            <ProductDetail />
-          </PrivateRoute>
+        <PrivateRoute path="/profile">
+          <ProfileForm />
+        </PrivateRoute>
 
-          <PrivateRoute path="/profile">
-            <ProfileForm />
-          </PrivateRoute>
-
-          <Redirect to="/" />
-        </Switch>
-      </div>
+        <Redirect to="/" />
+      </Switch>
+    </div>
   );
 }
 
