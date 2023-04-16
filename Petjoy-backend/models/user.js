cf5bf5e4a7682332f8673e0fdc934c5d6cb58bac
Cenk <bukercenk@gmail.com>
2023-04-15 21:04:01 -0500

    deployment

--- Petjoy-backend/models/user.js
+++ Petjoy-backend/models/user.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const db = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { sqlForPartialUpdate } = require("../helpers/sql");
 const {
   NotFoundError,
